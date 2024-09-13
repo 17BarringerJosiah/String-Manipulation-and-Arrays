9e7f235728a94469778a83b9d5126605d8d0831d
Josiah Barringer <59709224+17BarringerJosiah@users.noreply.github.com>
2024-09-13 18:02:38 -0400

    Add files via upload

---
+++ JB_Project4/JB_Project4/Form1.Designer.vb
@@ -0,0 +1,198 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lstMissions = New System.Windows.Forms.ListBox()
+        Me.lstTotals = New System.Windows.Forms.ListBox()
+        Me.txtHoursInput = New System.Windows.Forms.TextBox()
+        Me.lbl1 = New System.Windows.Forms.Label()
+        Me.btnAddHours = New System.Windows.Forms.Button()
+        Me.btnClose = New System.Windows.Forms.Button()
+        Me.txtChildrensMinistry = New System.Windows.Forms.TextBox()
+        Me.txtUsher = New System.Windows.Forms.TextBox()
+        Me.txtWorshipTeam = New System.Windows.Forms.TextBox()
+        Me.txtCommunityOutreach = New System.Windows.Forms.TextBox()
+        Me.lbl3 = New System.Windows.Forms.Label()
+        Me.lbl4 = New System.Windows.Forms.Label()
+        Me.lbl5 = New System.Windows.Forms.Label()
+        Me.lbl6 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lstMissions
+        '
+        Me.lstMissions.FormattingEnabled = True
+        Me.lstMissions.Location = New System.Drawing.Point(91, 96)
+        Me.lstMissions.Name = "lstMissions"
+        Me.lstMissions.Size = New System.Drawing.Size(220, 225)
+        Me.lstMissions.TabIndex = 0
+        '
+        'lstTotals
+        '
+        Me.lstTotals.FormattingEnabled = True
+        Me.lstTotals.Location = New System.Drawing.Point(339, 96)
+        Me.lstTotals.Name = "lstTotals"
+        Me.lstTotals.Size = New System.Drawing.Size(286, 225)
+        Me.lstTotals.TabIndex = 1
+        '
+        'txtHoursInput
+        '
+        Me.txtHoursInput.Location = New System.Drawing.Point(153, 70)
+        Me.txtHoursInput.Name = "txtHoursInput"
+        Me.txtHoursInput.Size = New System.Drawing.Size(100, 20)
+        Me.txtHoursInput.TabIndex = 2
+        '
+        'lbl1
+        '
+        Me.lbl1.AutoSize = True
+        Me.lbl1.Location = New System.Drawing.Point(163, 54)
+        Me.lbl1.Name = "lbl1"
+        Me.lbl1.Size = New System.Drawing.Size(79, 13)
+        Me.lbl1.TabIndex = 3
+        Me.lbl1.Text = "Hours Donated"
+        '
+        'btnAddHours
+        '
+        Me.btnAddHours.Location = New System.Drawing.Point(167, 327)
+        Me.btnAddHours.Name = "btnAddHours"
+        Me.btnAddHours.Size = New System.Drawing.Size(75, 23)
+        Me.btnAddHours.TabIndex = 4
+        Me.btnAddHours.Text = "&Add Hour s"
+        Me.btnAddHours.UseVisualStyleBackColor = True
+        '
+        'btnClose
+        '
+        Me.btnClose.Location = New System.Drawing.Point(447, 327)
+        Me.btnClose.Name = "btnClose"
+        Me.btnClose.Size = New System.Drawing.Size(75, 23)
+        Me.btnClose.TabIndex = 5
+        Me.btnClose.Text = "&Close"
+        Me.btnClose.UseVisualStyleBackColor = True
+        '
+        'txtChildrensMinistry
+        '
+        Me.txtChildrensMinistry.Location = New System.Drawing.Point(664, 84)
+        Me.txtChildrensMinistry.Name = "txtChildrensMinistry"
+        Me.txtChildrensMinistry.Size = New System.Drawing.Size(88, 20)
+        Me.txtChildrensMinistry.TabIndex = 8
+        '
+        'txtUsher
+        '
+        Me.txtUsher.Location = New System.Drawing.Point(664, 136)
+        Me.txtUsher.Name = "txtUsher"
+        Me.txtUsher.Size = New System.Drawing.Size(88, 20)
+        Me.txtUsher.TabIndex = 9
+        '
+        'txtWorshipTeam
+        '
+        Me.txtWorshipTeam.Location = New System.Drawing.Point(664, 194)
+        Me.txtWorshipTeam.Name = "txtWorshipTeam"
+        Me.txtWorshipTeam.Size = New System.Drawing.Size(88, 20)
+        Me.txtWorshipTeam.TabIndex = 10
+        '
+        'txtCommunityOutreach
+        '
+        Me.txtCommunityOutreach.Location = New System.Drawing.Point(664, 249)
+        Me.txtCommunityOutreach.Name = "txtCommunityOutreach"
+        Me.txtCommunityOutreach.Size = New System.Drawing.Size(88, 20)
+        Me.txtCommunityOutreach.TabIndex = 11
+        '
+        'lbl3
+        '
+        Me.lbl3.AutoSize = True
+        Me.lbl3.Location = New System.Drawing.Point(662, 68)
+        Me.lbl3.Name = "lbl3"
+        Me.lbl3.Size = New System.Drawing.Size(90, 13)
+        Me.lbl3.TabIndex = 12
+        Me.lbl3.Text = "Children's Ministry"
+        '
+        'lbl4
+        '
+        Me.lbl4.AutoSize = True
+        Me.lbl4.Location = New System.Drawing.Point(686, 120)
+        Me.lbl4.Name = "lbl4"
+        Me.lbl4.Size = New System.Drawing.Size(35, 13)
+        Me.lbl4.TabIndex = 13
+        Me.lbl4.Text = "Usher"
+        '
+        'lbl5
+        '
+        Me.lbl5.AutoSize = True
+        Me.lbl5.Location = New System.Drawing.Point(662, 178)
+        Me.lbl5.Name = "lbl5"
+        Me.lbl5.Size = New System.Drawing.Size(76, 13)
+        Me.lbl5.TabIndex = 14
+        Me.lbl5.Text = "Worship Team"
+        '
+        'lbl6
+        '
+        Me.lbl6.AutoSize = True
+        Me.lbl6.Location = New System.Drawing.Point(662, 233)
+        Me.lbl6.Name = "lbl6"
+        Me.lbl6.Size = New System.Drawing.Size(105, 13)
+        Me.lbl6.TabIndex = 15
+        Me.lbl6.Text = "Community Outreach"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.lbl6)
+        Me.Controls.Add(Me.lbl5)
+        Me.Controls.Add(Me.lbl4)
+        Me.Controls.Add(Me.lbl3)
+        Me.Controls.Add(Me.txtCommunityOutreach)
+        Me.Controls.Add(Me.txtWorshipTeam)
+        Me.Controls.Add(Me.txtUsher)
+        Me.Controls.Add(Me.txtChildrensMinistry)
+        Me.Controls.Add(Me.btnClose)
+        Me.Controls.Add(Me.btnAddHours)
+        Me.Controls.Add(Me.lbl1)
+        Me.Controls.Add(Me.txtHoursInput)
+        Me.Controls.Add(Me.lstTotals)
+        Me.Controls.Add(Me.lstMissions)
+        Me.KeyPreview = True
+        Me.Name = "Form1"
+        Me.Text = "Mission Hours"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lstMissions As ListBox
+    Friend WithEvents lstTotals As ListBox
+    Friend WithEvents txtHoursInput As TextBox
+    Friend WithEvents lbl1 As Label
+    Friend WithEvents btnAddHours As Button
+    Friend WithEvents btnClose As Button
+    Friend WithEvents txtChildrensMinistry As TextBox
+    Friend WithEvents txtUsher As TextBox
+    Friend WithEvents txtWorshipTeam As TextBox
+    Friend WithEvents txtCommunityOutreach As TextBox
+    Friend WithEvents lbl3 As Label
+    Friend WithEvents lbl4 As Label
+    Friend WithEvents lbl5 As Label
+    Friend WithEvents lbl6 As Label
+End Class
---
+++ JB_Project4/JB_Project4/Form1.vb
@@ -0,0 +1,96 @@
+﻿' STRING MANIPULATION AND ARRAYS PROGRAMMING ASSIGNMENT
+' To utilize string maniplulation and arrays
+' Josiah Barringer
+' I will not use code that was never covered in class Or in our textbook. If you do you must be
+' able to explain your code when asked by the professor. Using code outside of the
+' resources provided, it can be flagged And reported As an academic integrity violation
+' if there Is any suspicion Of copying/cheating
+
+
+Public Class Form1
+
+    ' Declare static array to store hours for each mission
+    Dim missionHours(3) As Integer
+    ' Declare string array for mission names
+    Dim missionNames() As String = {"Children's Ministry", "Usher Ministry", "Worship Team Ministry", "Community Outreach Ministry"}
+
+    ' Form load event to initialize form data
+    Private Sub FrmMissionHours_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Clear the listbox for donations
+        lstTotals.Items.Clear()
+        ' Load mission names into the mission listbox
+        lstMissions.Items.AddRange(missionNames)
+        ' Display the current date
+        lstTotals.Items.Add("Date: " & DateTime.Now.ToString("MM/dd/yyyy"))
+        ' Display user name
+        lstTotals.Items.Add("Josiah Barringer")
+    End Sub
+
+    ' Add Hours Button Click event
+    Private Sub BtnAddHours_Click(sender As Object, e As EventArgs) Handles btnAddHours.Click
+
+        ' Validate user input for hours and mission selection
+        If txtHoursInput.Text = String.Empty Then
+            MessageBox.Show("Please enter the number of hours.", "Input Error")
+            txtHoursInput.Focus()
+            Exit Sub
+        ElseIf Not IsNumeric(txtHoursInput.Text) OrElse CInt(txtHoursInput.Text) <= 0 Then
+            MessageBox.Show("Please enter a valid positive number of hours.", "Input Error")
+            txtHoursInput.Focus()
+            Exit Sub
+        End If
+
+        If lstMissions.SelectedIndex = -1 Then
+            MessageBox.Show("Please select a mission.", "Input Error")
+            lstMissions.Focus()
+            Exit Sub
+        End If
+
+        ' Get the number of hours and the selected mission index
+        Dim hours As Integer = CInt(txtHoursInput.Text)
+        Dim missionIndex As Integer = lstMissions.SelectedIndex
+
+        ' Add hours to the appropriate mission in the array
+        missionHours(missionIndex) += hours
+
+        ' Get the user's name using an input box
+        Dim fullName As String = InputBox("Enter you full name:", "Enter Name")
+        If fullName = String.Empty Then
+            MessageBox.Show("Name entry is required.", "Input Error")
+            Exit Sub
+        End If
+
+        ' Extract the last name from the full name
+        Dim lastName As String = GetLastName(fullName)
+
+        ' Display the name and hours served in the listbox
+        lstTotals.Items.Add("_________________")
+        lstTotals.Items.Add(lastName & " served " & hours & " hours in the " & missionNames(missionIndex) & ".")
+
+        ' Update the running totals for each mission in the corresponding labels
+        txtChildrensMinistry.Text = missionHours(0).ToString()
+        txtUsher.Text = missionHours(1).ToString()
+        txtWorshipTeam.Text = missionHours(2).ToString()
+        txtCommunityOutreach.Text = missionHours(3).ToString()
+
+        ' Clear the hours textbox and reset the mission listbox selection
+        txtHoursInput.Clear()
+        lstMissions.ClearSelected()
+
+    End Sub
+
+    ' Function to return just the last name from a full name
+    Private Function GetLastName(fullName As String) As String
+        ' Split the full name by space and return the last part
+        Dim nameParts() As String = fullName.Split(" "c)
+        Return nameParts(nameParts.Length - 1)
+    End Function
+
+    ' Close Button Click event to close the application
+    Private Sub BtnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+        Me.Close()
+    End Sub
+
+End Class
+
+
